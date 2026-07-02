9491c0b4abf903021d26ed43f01657f3157aa413
Perry <perry.ivic@hotmail.com>
2026-07-02 19:55:08 +0200

    Added pages for archives and implemented grids

---
+++ src/project-data/game-jam-projects.ts
@@ -0,0 +1,34 @@
+import icon from "../assets/entry-border-icon.png";
+
+export const featuredProjects = [
+    {
+        image: icon,
+        title: "Loot Goblin",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/soft-body-physics"
+    },
+    {
+        image: icon,
+        title: "Dragon Horde",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/mesh-slicer"
+    },
+    {
+        image: icon,
+        title: "Trespasser",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/soft-body-physics"
+    },
+    {
+        image: icon,
+        title: "Die or Die",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/mesh-slicer"
+    },
+    {
+        image: icon,
+        title: "Let Them Eat Fist",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/soft-body-physics"
+    },
+];
---
+++ src/project-data/tga-specialization.ts
@@ -0,0 +1,10 @@
+import icon from "../assets/entry-border-icon.png";
+
+export const specializations = [
+    {
+        image: icon,
+        title: "SHOTGUN PARKOUR",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/soft-body-physics"
+    },
+];
