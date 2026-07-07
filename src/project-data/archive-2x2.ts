306a29ee523329532e2108753e87494e9d52e1e7
Perry <perry.ivic@hotmail.com>
2026-07-07 20:56:33 +0200

    added archive page

---
+++ src/project-data/archive-2x2.ts
@@ -0,0 +1,21 @@
+import GameJamImage from "../assets/game-jams/game-jams.png"
+import TgaProjectsImage from "../assets/tga-projects/tga-projects.png"
+
+export const projects2x2 = [
+    {
+        title: "Game Jams",
+        subtitle: "C++ • OpenGL",
+        href: "/archive/game-jams/game-jams",
+        image: GameJamImage,
+        video: "/game-jam-reel.webm",
+        objectFit: "contain"
+    },
+    {
+        title: "TGA Projects",
+        subtitle: "C++ • OpenGL",
+        href: "/archive/tga-projects/tga-projects",
+        image: TgaProjectsImage,
+        video: "/tga-projects-reel.webm",
+        objectFit: "contain"
+    },
+];
