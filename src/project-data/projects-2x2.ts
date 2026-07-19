8d8d2639df8dcc530f4b5f067013e90b8144114d
Perry <perry.ivic@hotmail.com>
2026-07-19 16:04:37 +0200

    Created entry for mesh slicer

---
+++ src/project-data/projects-2x2.ts
@@ -0,0 +1,21 @@
+import MeshSlicerImage from "../assets/projects/mesh-slicer/mesh-slicer-cover.png"
+import TgaProjectsImage from "../assets/tga-projects/tga-projects.png"
+
+export const projects2x2 = [
+    {
+        title: "TGA Projects",
+        subtitle: "C++ • OpenGL",
+        href: "/archive/tga-projects/tga-projects",
+        image: TgaProjectsImage,
+        video: "/tga-projects-reel.webm",
+        objectFit: "cover"
+    },
+        {
+        title: "Mesh Slicer",
+        subtitle: "C++ • OpenGL",
+        href: "/projects/mesh-slicer",
+        image: MeshSlicerImage,
+        video: "/mesh-slicer/thanks-for-watching.webm",
+        objectFit: "cover"
+    },
+];
