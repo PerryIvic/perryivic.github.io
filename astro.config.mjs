8aa3ee8b50e11036df1e4c180d51c9a741ede8af
PerryIvic <perry.ivic@studerande.thegameassembly.se>
2026-08-04 11:08:03 +0200

    Installed astro icon extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import preact from "@astrojs/preact";
 
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.perryivic.github.io",
-  integrations: [preact(), mdx()]
+  integrations: [preact(), mdx(), icon()]
 });
